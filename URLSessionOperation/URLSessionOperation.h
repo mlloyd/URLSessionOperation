7fc2dacd99d4678f65e06108be9dcf251104dd8f
Martin Lloyd <martin.lloyd@thomsonreuters.com>
2015-10-29 10:45:18 +0000

    Initial Commit

---
+++ URLSessionOperation/URLSessionOperation.h
@@ -0,0 +1,19 @@
+//
+//  URLSessionOperation.h
+//  URLSessionOperation
+//
+//  Created by Martin Lloyd on 29/10/2015.
+//  Copyright © 2015 Martin Lloyd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for URLSessionOperation.
+FOUNDATION_EXPORT double URLSessionOperationVersionNumber;
+
+//! Project version string for URLSessionOperation.
+FOUNDATION_EXPORT const unsigned char URLSessionOperationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <URLSessionOperation/PublicHeader.h>
+
+
